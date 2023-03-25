9027342b8ca949c0f60c0588f0856ea8d2e175f6
aSquare <artur.anissimov@gmail.com>
2023-03-25 17:26:26 +0200

    Use Unique Keys to Avoid Duplication

--- database/migrations/2023_03_25_123703_post_tag.php
+++ database/migrations/2023_03_25_123703_post_tag.php
@@ -16,6 +16,8 @@ return new class extends Migration {
             $table->unsignedBigInteger("tag_id");
             $table->timestamps();
 
+            $table->unique(["post_id", "tag_id"]);
+
             $table
                 ->foreign("post_id")
                 ->references("id")
